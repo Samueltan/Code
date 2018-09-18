85377230102a861920ae1694016fcafc54c7d436
Samuel Tan <samueltango@gmail.com>
2018-09-18 18:41:14 -0400

    Use AtomicInteger

--- Sample/src/com/sample/basic/thread/volatiletest/VolatileTest.java
+++ Sample/src/com/sample/basic/thread/volatiletest/VolatileTest.java
@@ -1,19 +1,21 @@
-package com.sample.basic.thread;
+package com.sample.basic.thread.volatiletest;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class VolatileTest {
-    static volatile int count = 0;
+    static AtomicInteger count = new AtomicInteger(0);
 
     public static void main(String[] args) {
         // 线程1
         new Thread(()->{
-            do{}while (count<1);
+            do{}while (count.get()<1);
             System.out.println("exit when count = "+count);
         }).start();
         // 线程2
         new Thread(()->{
             try {
                 Thread.sleep(1000);
-                count = 10;
+                count.set(10);
                 System.out.println("change value of count to "+count);
             } catch (InterruptedException e) {
                 e.printStackTrace();
