acb779b446e434c3b7508fd1f279bc930a4f8949
Samuel Tan <samueltango@gmail.com>
2018-09-18 15:37:22 -0400

    Add Angular2 project

--- Sample/src/com/sample/basic/Test.java
+++ Sample/src/com/sample/basic/Test.java
@@ -2,13 +2,16 @@ package com.sample.basic;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {
-        Integer[] t = {1,2,3,4,5};
-        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(t));
-
-        for(Integer i : list) {
-            System.out.println(i);
-        }
+    private static int nonLeadingZeroComplement(int i) {
+        int ones = (Integer.highestOneBit(i) << 1) - 1;
+        return i ^ ones;
     }
 
+    // Driver Code
+    public static void main (String[] args)
+    {
+        int result = nonLeadingZeroComplement(50);
+
+        System.out.println(result);
+    }
 }
---
+++ Sample/src/com/sample/basic/interview/Complement.java
@@ -0,0 +1,21 @@
+package com.sample.basic.interview;
+
+public class Complement {
+    private static int nonLeadingZeroComplement(int i) {
+        int ones = (Integer.highestOneBit(i) << 1) - 1;
+        return i ^ ones;
+    }
+
+    // Driver Code
+    public static void main (String[] args)
+    {
+        int i = 66;
+        System.out.println("i = " + Integer.toBinaryString(i));
+        System.out.println("highestOneBit(i) = " + Integer.toBinaryString(Integer.highestOneBit(i)));
+        System.out.println("highestOneBit(i) << 1 = " + Integer.toBinaryString(Integer.highestOneBit(i) << 1));
+        int ones = (Integer.highestOneBit(i) << 1) - 1;
+        System.out.println("(highestOneBit(i) << 1) - 1= " + Integer.toBinaryString(ones));
+        int result = i ^ ones;
+        System.out.println("i ^ (highestOneBit(i) << 1) - 1= " + Integer.toBinaryString(result));
+    }
+}
