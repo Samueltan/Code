8a189814ce9aa98b5e78d5393f6087be9f639f67
Samuel Tan <samueltango@gmail.com>
2018-09-23 14:55:59 -0400

    docker example, jdk8 stream test

--- Sample/src/com/sample/Test.java
+++ Sample/src/com/sample/Test.java
@@ -1,13 +1,8 @@
 package com.sample;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class Test {
     private static boolean stopRequest;
     public static void main(String args[]) throws InterruptedException {
-        List<String> emptyImmutableList = List.of();
 
         int i = sum(1, 2, 3, 4, 5);
         System.out.println("i = " + i);
