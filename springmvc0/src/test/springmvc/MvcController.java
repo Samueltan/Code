ed4cbb06fb763c11f72cd5d3d52475c271d3617c
Samuel Tan <samueltango@gmail.com>
2018-10-04 18:14:03 -0400

    no message

--- springmvc0/src/test/springmvc/MvcController.java
+++ springmvc0/src/test/springmvc/MvcController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mvc")
+//@RequestMapping("/mvc")
 public class MvcController {
 
     @RequestMapping("/hello")
