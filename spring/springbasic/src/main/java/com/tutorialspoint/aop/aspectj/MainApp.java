ce481b04e25efeb074bf8053dd9bc85ec83c5ca4
Samuel Tan <samueltango@gmail.com>
2018-10-14 00:12:40 -0400

    use aspectj annotation

--- spring/springbasic/src/main/java/com/tutorialspoint/aop/aspectj/MainApp.java
+++ spring/springbasic/src/main/java/com/tutorialspoint/aop/aspectj/MainApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("BeansAop.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("BeansAopAspectj.xml");
         Student student = (Student) context.getBean("student");
         student.getName();
         student.getAge();
