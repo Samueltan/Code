453859d6ad39d1ddeee28dbe6c642222e72510b4
Samuel Tan <samueltango@gmail.com>
2018-10-15 19:03:18 -0400

    Spring mvc hello from sctrach without xml

--- spring/springbasic/src/main/java/com/tutorialspoint/config/java/AppConfig.java
+++ spring/springbasic/src/main/java/com/tutorialspoint/config/java/AppConfig.java
@@ -6,34 +6,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan("com.tutorialspoint.config.java")
+//@ComponentScan("com.tutorialspoint.config.java")
 public class AppConfig {
     /**
      * All the below @Bean definitions could be removed if @ComponentScan is used and all beans are
      * marked with proper annotations (@Component, @Controller, @Service, etc.)
      */
-//    @Bean
-//    public HelloWorld helloWorld(){
-//        return new HelloWorld();
-//    }
-//
-//    @Bean
-//    public TextEditor textEditor(){
-//        return new TextEditor(spellChecker());
-//    }
-//
-//    @Bean
-//    public SpellChecker spellChecker(){
-//        return new SpellChecker();
-//    }
-//
-//    @Bean
-//    public CStartEventHandler cStartEventHandler() {
-//        return new CStartEventHandler();
-//    }
-//
-//    @Bean
-//    public CStopEventHandler cStopEventHandler() {
-//        return new CStopEventHandler();
-//    }
+    @Bean
+    public HelloWorld helloWorld(){
+        return new HelloWorld();
+    }
+
+    @Bean
+    public TextEditor textEditor(){
+        return new TextEditor(spellChecker());
+    }
+
+    @Bean
+    public SpellChecker spellChecker(){
+        return new SpellChecker();
+    }
+
 }
