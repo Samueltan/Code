64891778c8c803bde079b3f6f0533fd24fb3a373
Samuel Tan <samueltango@gmail.com>
2018-10-22 23:33:41 -0400

    Write to csv

--- Sample/src/main/java/com/sample/interview/mimecast/FileSearcher.java
+++ Sample/src/main/java/com/sample/interview/mimecast/FileSearcher.java
@@ -10,6 +10,7 @@ import org.apache.commons.cli.ParseException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -28,11 +29,31 @@ import java.util.regex.Pattern;
 
 public class FileSearcher {
     private final static int THREADPOOL_SIZE = 20;
-    private final static int QUEUE_LIMIT = 1024;
+    private final static int QUEUE_LIMIT = 64;
     private final static int BUFFER_SIZE = 65536;
+    private static ConcurrentLinkedQueue<String> queue =  new ConcurrentLinkedQueue<>();
+    static final ExecutorService executorService = Executors.newFixedThreadPool(THREADPOOL_SIZE);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
+        // Parse arguments for command options
+        CommandLine cmd = parseArgs(args);
 
+        String folderPath = cmd.getOptionValue("path");
+        String searchTerm = cmd.getOptionValue("search");
+        String outputPath = cmd.getOptionValue("output");
+
+        // Search the string pattern in the given folder
+        searchPatternInFolder(folderPath, searchTerm);
+
+        // Write the result from buffer to csv file
+        writeResultToCsv(outputPath);
+
+        executorService.shutdown();
+    }
+
+    private static CommandLine parseArgs(String[] args)
+    {
         Options options = new Options();
 
         Option folder = new Option("p", "path", true, "the path of the folder to scan");
@@ -49,33 +70,24 @@ public class FileSearcher {
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
+        CommandLine cmd = null;
         try {
             cmd = parser.parse(options, args);
-
-            String folderPath = cmd.getOptionValue("path");
-            String searchTerm = cmd.getOptionValue("search");
-            String outputPath = cmd.getOptionValue("output");
-
-            searchPatternInFolder(folderPath, searchTerm, outputPath);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
             formatter.printHelp("search string patterns in a given folder", options);
 
             System.exit(1);
         }
+
+        return cmd;
     }
 
     private static void searchPatternInFolder(
         final String folderPath,
-        final String searchTerm,
-        final String outputPath
+        final String searchTerm
     )
     {
-        final ExecutorService es = Executors.newFixedThreadPool(THREADPOOL_SIZE);
-        ConcurrentLinkedQueue<String> queue =  new ConcurrentLinkedQueue<>();
-
         try {
             Files.walkFileTree(
                 Paths.get(folderPath),
@@ -84,34 +96,34 @@ public class FileSearcher {
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                         CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(
                             () -> {
-                                return searchInFile(file, searchTerm, queue);
+                                return searchInFile(file, searchTerm);
                             },
-                            es
+                                executorService
                         );
 
                         completableFuture.whenComplete(
-                                (result, e) -> {
-                                    if(result > 0) {
-                                        System.out.println(file + ": " + result);
-                                    }
+                            (result, e) -> {
+                                if(result > 0) {
+                                    System.out.println(file + ", " + result);
+                                    queue.add(file + ", " + result + "\n");
                                 }
+                            }
                         );
 
                         return FileVisitResult.CONTINUE;
                     }
                 }
             );
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        es.shutdown();
     }
 
     private static int searchInFile(
         final Path file,
-        final String searchTerm,
-        final ConcurrentLinkedQueue<String> queue
+        final String searchTerm
     )
     {
         int count = 0;
@@ -150,4 +162,29 @@ public class FileSearcher {
         return count;
     }
 
+    private static void writeResultToCsv(String outputPath)
+    {
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(
+                () -> {
+                    FileOutputStream out = null;
+                    try {
+                        out = new FileOutputStream(outputPath);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    while(true)
+                    {
+                        if(!queue.isEmpty()){
+                            try {
+                                out.write(queue.poll().getBytes());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                executorService
+        );
+    }
 }
