13970d9255987949a2111f863c29bc615a58775e
Samuel Tan <samueltango@gmail.com>
2019-01-31 15:58:11 -0500

    refine

--- angular/toh-pt6/src/app/app.component.ts
+++ angular/toh-pt6/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { colorSets } from './ngx-charts/utils/color-sets';
-import { bubble } from './ngx-charts/demo/data';
-import chartGroups from './ngx-charts/demo/chartTypes';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -71,23 +67,155 @@ export class AppComponent implements OnInit {
   rangeFillOpacity: number = 0.15;
 
   constructor() {
-
-    Object.assign(this, {
-      chartGroups,
-      colorSets,
-      bubble
-    });
-
-    this.setColorScheme('cool');
   }
 
   ngOnInit() {
     this.view = [this.width, this.height];
-  }
 
-  setColorScheme(name) {
-    this.selectedColorScheme = name;
-    this.colorScheme = this.colorSets.find(s => s.name === name);
+    this.colorScheme = {
+        name: 'cool',
+        selectable: true,
+        group: 'Ordinal',
+        domain: [
+          '#a8385d', '#7aa3e5', '#a27ea8', '#aae3f5', '#adcded', '#a95963', '#8796c0', '#7ed3ed', '#50abcc', '#ad6886'
+        ]
+    };
+    this.bubble = [
+        {
+          name: 'Germany',
+          series: [
+            {
+              name: '2010',
+              x: new Date(2010, 0, 1),
+              y: 80.3,
+              r: 80.4
+            },
+            {
+              name: '2000',
+              x: new Date(2000, 0, 1),
+              y: 80.3,
+              r: 78
+            },
+            {
+              name: '1990',
+              x: new Date(1990, 0, 1),
+              y: 75.4,
+              r: 79
+            }
+          ]
+        },
+        {
+          name: 'United States',
+          series: [
+            {
+              name: '2010',
+              x: new Date(2010, 0, 1),
+              y: 78.8,
+              r: 310
+            },
+            {
+              name: '2000',
+              x: new Date(2000, 0, 1),
+              y: 76.9,
+              r: 283
+            },
+            {
+              name: '1990',
+              x: new Date(1990, 0, 1),
+              y: 75.4,
+              r: 253
+            }
+          ]
+        },
+        {
+          name: 'France',
+          series: [
+            {
+              name: '2010',
+              x: new Date(2010, 0, 1),
+              y: 81.4,
+              r: 63
+            },
+            {
+              name: '2000',
+              x: new Date(2000, 0, 1),
+              y: 79.1,
+              r: 59.4
+            },
+            {
+              name: '1990',
+              x: new Date(1990, 0, 1),
+              y: 77.2,
+              r: 56.9
+            }
+          ]
+        },
+        {
+          name: 'United Kingdom',
+          series: [
+            {
+              name: '2010',
+              x: new Date(2010, 0, 1),
+              y: 80.2,
+              r: 62.7
+            },
+            {
+              name: '2000',
+              x: new Date(2000, 0, 1),
+              y: 77.8,
+              r: 58.9
+            },
+            {
+              name: '1990',
+              x: new Date(1990, 0, 1),
+              y: 75.7,
+              r: 57.1
+            }
+          ]
+        }
+      ];
+
+      this.chartGroups = [
+
+        {
+          name: 'Other Charts',
+          charts: [
+            {
+              name: 'Bubble Chart',
+              selector: 'bubble-chart',
+              inputFormat: 'bubble',
+              options: [
+                'animations',
+                'colorScheme',
+                'schemeType',
+                'showXAxis',
+                'showYAxis',
+                'showLegend',
+                'legendTitle',
+                'legendPosition',
+                'showXAxisLabel',
+                'xAxisLabel',
+                'showYAxisLabel',
+                'yAxisLabel',
+                'showGridLines',
+                'roundDomains',
+                'autoScale',
+                'minRadius',
+                'maxRadius',
+                'tooltipDisabled',
+                'xScaleMin',
+                'xScaleMax',
+                'yScaleMin',
+                'yScaleMax'
+              ],
+              defaults: {
+                xAxisLabel: 'Census Date',
+                yAxisLabel: 'Life expectancy [years]'
+              }
+            }
+          ]
+        }
+      ];
   }
   
 }
