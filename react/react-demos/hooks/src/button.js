916330b380792b4440675fe4c8fddd8b06b20cc7
JTan <jtan@cygilant.com>
2020-03-04 17:19:34 -0500

    Render Props

--- react/react-demos/hooks/src/button.js
+++ react/react-demos/hooks/src/button.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Button() {
   const [text, setButtonText] = useState("On");
@@ -7,6 +7,10 @@ export default function Button() {
     const txt = text === "On" ? "Off" : "On";
     return setButtonText(txt);
   }
+
+  useEffect(() => {
+    document.title = `You changed title to ${text}`;
+  });
 
   return <button onClick={handleClick}>{text}</button>;
 }
