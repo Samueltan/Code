658aaa0ef532aaf84798783274e90054a8a1b10e
Samuel Tan <samueltango@gmail.com>
2020-12-18 16:57:55 -0500

    Consume rest service with RestTemplate

--- spring/tutorial/demo/src/main/java/spring/demo/DemoApplication.java
+++ spring/tutorial/demo/src/main/java/spring/demo/DemoApplication.java
@@ -1,13 +1,40 @@
 package spring.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import spring.demo.model.Quote;
 
 @SpringBootApplication
 public class DemoApplication {
+	public static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+//	@Bean
+//	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//		return builder.build();
+//	}
+
+	@Bean
+	public CommandLineRunner run(ApplicationContext ctx) throws Exception {
+		return args -> {
+			RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+			Quote quote = restTemplate.getForObject(
+				"https://gturnquist-quoters.cfapps.io/api/random",
+					Quote.class
+			);
+			log.info(quote.toString());
+		};
 	}
 
 //	@Bean
