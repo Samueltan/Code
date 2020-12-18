eea76a2c37fcfa2bd81112d7fccb5f6abf7ec2c8
Samuel Tan <samueltango@gmail.com>
2020-12-18 01:51:39 -0500

    Test RequestParam and add pom.xml back

--- spring/tutorial/demo/src/main/java/spring/demo/controller/HomeController.java
+++ spring/tutorial/demo/src/main/java/spring/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,8 +13,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
     @RequestMapping(value="/hi", method = RequestMethod.GET)
     @ResponseBody
-    public String hi() {
-        return "Hello, Spring Boot!";
+    public String hi(
+        @RequestParam(value="name", defaultValue = "world")
+        String name
+    ) {
+        return String.format("Hello, %s!", name);
     }
 
     @RequestMapping("/greeting")
