fcc680dbccce19948e864b8bfc5ccc8ca3224a20
Samuel Tan <samueltango@gmail.com>
2021-01-09 18:46:01 -0500

    Add add tester

---
+++ Java/spring/tutorial/demo/src/test/java/mockito/MathApplicationTester.java
@@ -0,0 +1,26 @@
+package mockito;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MathApplicationTester {
+    @InjectMocks
+    MathApplication mathApplication = new MathApplication();
+
+    @Mock
+    CalculatorService calculatorService;
+
+    @Test
+    public void testAdd() {
+        when(calculatorService.add(10.0, 20.0)).thenReturn(30.0);
+
+        Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+    }
+}
