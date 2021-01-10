1b5926f23590d9cf570c69b5cb82033805edc31f
Samuel Tan <samueltango@gmail.com>
2021-01-10 01:07:59 -0500

    use spy

--- Java/spring/tutorial/demo/src/test/java/mockito/MathApplicationTester.java
+++ Java/spring/tutorial/demo/src/test/java/mockito/MathApplicationTester.java
@@ -1,49 +1,73 @@
 package mockito;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MathApplicationTester {
-    @InjectMocks
-    MathApplication mathApplication = new MathApplication();
+    MathApplication mathApplication;
+    CalculatorService calculatorService1;
+    CalculatorService calculatorService2;
 
-    @Mock
-    CalculatorService calculatorService;
+    @Before
+    public void setUp() {
+        mathApplication = new MathApplication();
+        calculatorService1 = mock(CalculatorService.class);
+
+        Calculator calculator = new Calculator();
+        calculatorService2 = spy(calculator);
+    }
 
     @Test
     public void testAddAndSubtract() {
-        when(calculatorService.add(10.0, 20.0)).thenReturn(30.0);
-        when(calculatorService.subtract(10.0, 20.0)).thenReturn(-10.0);
+        mathApplication.setCalculatorService(calculatorService1);
+        when(calculatorService1.add(10.0, 20.0)).thenReturn(30.0);
+        when(calculatorService1.subtract(10.0, 20.0)).thenReturn(-10.0);
 
         var sum = mathApplication.add(10.0, 20.0);
         Assert.assertEquals(sum, 30.0, 0);
         var diff = mathApplication.subtract(10.0, 20.0);
         Assert.assertEquals(diff, -10.0, 0);
 
-        InOrder inOrder = inOrder(calculatorService);
+        InOrder inOrder = inOrder(calculatorService1);
 
-        inOrder.verify(calculatorService).add(10.0, 20.0);
-        inOrder.verify(calculatorService).subtract(10.0, 20.0);
+        inOrder.verify(calculatorService1).add(10.0, 20.0);
+        inOrder.verify(calculatorService1).subtract(10.0, 20.0);
     }
 
     @Test(expected = RuntimeException.class)
     public void testAddException() {
-        doThrow(new RuntimeException("Add operation not implemented")).when(calculatorService).add(10.0, 20.0);
+        mathApplication.setCalculatorService(calculatorService1);
+        doThrow(new RuntimeException("Add operation not implemented")).when(calculatorService1).add(10.0, 20.0);
 
         var result = mathApplication.add(10.0, 20.0);
-        Assert.assertEquals(result, 30.0, 0);
-
-        verify(calculatorService).add(10.0, 20.0);
     }
+
+    @Test
+    public void testAdd() {
+        mathApplication.setCalculatorService(calculatorService2);
+
+        var sum = mathApplication.add(10.0, 20.0);
+        Assert.assertEquals(sum, 30.0, 0);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testSubtract() {
+        mathApplication.setCalculatorService(calculatorService2);
+
+        var sum = mathApplication.subtract(10.0, 20.0);
+    }
+
 }
